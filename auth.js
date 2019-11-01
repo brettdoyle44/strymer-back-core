b367677513136339ba48e0d7b58617ed118cb085
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-11-01 14:57:27 -0400

    Further abstraction of env vars

--- auth.js
+++ auth.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const request = require('request');
-const cors = require('cors');
+const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const request = require('request')
+const cors = require('cors')
 
-const { OAuth2Strategy } = require('passport-oauth');
+const { OAuth2Strategy } = require('passport-oauth')
 
-const TWITCH_CLIENT_ID = '1goezo7ijjizi65ftuafta782iuipv';
-const TWITCH_SECRET = 'nd94ig7ln2gslg090ks9k45eiustww';
-const SESSION_SECRET = 'justputitrightinthere';
-const CALLBACK_URL = 'https://strymer.stream/api/auth/twitch/callback';
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID
+const TWITCH_SECRET = process.env.TWITCH_SECRET
+const SESSION_SECRET = process.env.SESSION_SECRET
+const CALLBACK_URL = process.env.CALLBACK_URL
 
-export const twitchAuth = express();
+export const twitchAuth = express()
 twitchAuth.use(
-  session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }),
-);
-twitchAuth.use(express.static('public'));
-twitchAuth.use(passport.initialize());
-twitchAuth.use(passport.session());
+  session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
+)
+twitchAuth.use(express.static('public'))
+twitchAuth.use(passport.initialize())
+twitchAuth.use(passport.session())
 
 OAuth2Strategy.prototype.userProfile = (accessToken, done) => {
   const options = {
@@ -26,26 +26,26 @@ OAuth2Strategy.prototype.userProfile = (accessToken, done) => {
     headers: {
       'Client-ID': TWITCH_CLIENT_ID,
       Accept: 'application/vnd.twitchtv.v5+json',
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
+      Authorization: `Bearer ${accessToken}`
+    }
+  }
 
   request(options, (error, response, body) => {
     if (response && response.statusCode === 200) {
-      done(null, JSON.parse(body));
+      done(null, JSON.parse(body))
     } else {
-      done(JSON.parse(body));
+      done(JSON.parse(body))
     }
-  });
-};
+  })
+}
 
 passport.serializeUser((user, done) => {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.use(
   'twitch',
@@ -56,37 +56,35 @@ passport.use(
       clientID: TWITCH_CLIENT_ID,
       clientSecret: TWITCH_SECRET,
       callbackURL: CALLBACK_URL,
-      state: true,
+      state: true
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('profile', profile);
-      profile.accessToken = accessToken;
-      profile.refreshToken = refreshToken;
+      console.log('profile', profile)
+      profile.accessToken = accessToken
+      profile.refreshToken = refreshToken
 
-      done(null, profile);
-    },
-  ),
-);
+      done(null, profile)
+    }
+  )
+)
 
 twitchAuth.get(
   '/api/auth/twitch',
-  passport.authenticate('twitch', { scope: 'user_read' }),
-);
+  passport.authenticate('twitch', { scope: 'user_read' })
+)
 
 twitchAuth.get(
   '/api/auth/twitch/callback',
   passport.authenticate('twitch', {
-    successRedirect: '/api/twitch/main',
-  }),
-);
+    successRedirect: '/api/twitch/main'
+  })
+)
 
 // needs to be strymer-test-new.herokuapp.com or production when deployed
 twitchAuth.get('/api/twitch/main', cors(), (req, res) => {
   if (req.session && req.session.passport && req.session.passport.user) {
     res.redirect(
-      `https://strymer.gg/profile?${req.session.passport.user.data[0].id}--${
-        req.session.passport.user.accessToken
-      }`,
-    );
+      `https://strymer.gg/profile?${req.session.passport.user.data[0].id}--${req.session.passport.user.accessToken}`
+    )
   }
-});
+})
--- index.js
+++ index.js
@@ -1,97 +1,95 @@
-import { } from 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import bodyParser from 'body-parser';
-import { theIDs } from './ids.json';
-import { twitchAuth } from './auth';
+import {} from 'dotenv/config'
+import cors from 'cors'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { theIDs } from './ids.json'
+import { twitchAuth } from './auth'
 
-const fs = require('fs');
+const fs = require('fs')
 // TWITCH APIS/IDS
-const api = require('twitch-api-v5');
-const axios = require('axios');
-const ffmpeg = require('fluent-ffmpeg');
-const fetch = require('node-fetch');
+const api = require('twitch-api-v5')
+const axios = require('axios')
+const ffmpeg = require('fluent-ffmpeg')
+const fetch = require('node-fetch')
 
-const clientID = process.env.CLIENT_ID;
-const twitchStreams = require('twitch-get-stream')(clientID);
+const clientID = process.env.CLIENT_ID
+const twitchStreams = require('twitch-get-stream')(clientID)
 
-const app = express();
+const app = express()
 
 const corsOptions = [
   'http://localhost:1234',
   'https://strymer.gg',
-  'https://www.strymer.gg',
-];
+  'https://www.strymer.gg'
+]
 
 app
   .use('*', cors(corsOptions))
   .use(bodyParser.json())
   .use(twitchAuth)
   .get('/api/get-playlist', (req, res) => {
-    const { stream, quality } = req.query;
+    const { stream, quality } = req.query
 
     const getRawURL = streamName =>
       twitchStreams
         .rawParsed(streamName)
         .then(back => back.filter(s => s.title.includes('audio_only'))[0].file)
         .catch(err => {
-          console.log('ERROR GETTING TWITCH STREAM -- POSSIBLY PRIVATE?', err);
-        });
+          console.log('ERROR GETTING TWITCH STREAM -- POSSIBLY PRIVATE?', err)
+        })
 
     if (
       fs.existsSync(
-        `/var/www/strymer.dev/html/${stream}-${quality || 'raw'}.m3u8`,
+        `/var/www/strymer.dev/html/${stream}-${quality || 'raw'}.m3u8`
       )
     ) {
-      console.log('THIS PLAYLIST ALREADY EXISTS!');
-      res.json(`https://strymer.dev/${stream}-${quality || 'raw'}.m3u8`);
+      console.log('THIS PLAYLIST ALREADY EXISTS!')
+      res.json(`https://strymer.dev/${stream}-${quality || 'raw'}.m3u8`)
     } else {
-      console.log('GETTING PLAYLIST FILE');
+      console.log('GETTING PLAYLIST FILE')
 
       getRawURL(stream)
         .then(async audioOnly => {
           const audioToStream = !audioOnly
             ? `/var/www/strymer.dev/html/${stream}.m3u8`
-            : audioOnly;
+            : audioOnly
 
-          const bitRate = !quality ? 320 : quality;
+          const bitRate = !quality ? 320 : quality
           ffmpeg(audioToStream, { timeout: 0 })
             .audioBitrate(bitRate)
             .noVideo()
             .addOptions(['-hls_flags delete_segments'])
             .on('error', err => console.log('ERRPR!@', err))
-            .save(`/var/www/strymer.dev/html/${stream}-${quality}.m3u8`);
+            .save(`/var/www/strymer.dev/html/${stream}-${quality}.m3u8`)
         })
         .then(() => {
-          res.json(`https://strymer.dev/${stream}-${quality}.m3u8`);
+          res.json(`https://strymer.dev/${stream}-${quality}.m3u8`)
         })
         .catch(err =>
-          console.log('There was an error sending the encoded file back', err),
-        );
+          console.log('There was an error sending the encoded file back', err)
+        )
     }
   })
 
   .get('/api/meta', (req, res) => {
-    api.clientID = clientID;
+    api.clientID = clientID
 
     const streamInfo = theIDs.map(id =>
       axios
         .get(`https://api.twitch.tv/helix/streams/${id}`, {
           headers: {
             'Client-ID': clientID
-          },
+          }
         })
-        .then(v => v.data.stream),
-    );
+        .then(v => v.data.stream)
+    )
 
     Promise.all(streamInfo).then(streams =>
       res.json(
         streams
           .filter(x => x)
           .map(s => {
-            const {
-              name, logo, url, display_name, status, game
-            } = s.channel;
+            const { name, logo, url, display_name, status, game } = s.channel
 
             return {
               stream: name,
@@ -99,16 +97,16 @@ app
               game,
               url,
               title: display_name,
-              status,
-            };
-          }),
-      ),
-    );
+              status
+            }
+          })
+      )
+    )
   })
 
   .post('/api/tourney', (req, res) => {
-    const { search } = req.body;
-    console.log('SEA', search);
+    const { search } = req.body
+    console.log('SEA', search)
 
     axios
       .get(
@@ -116,19 +114,17 @@ app
         {
           headers: {
             'Client-ID': clientID
-          },
-        },
+          }
+        }
       )
       .then(raw => {
-        console.log('RAW', raw);
+        console.log('RAW', raw)
         const isStream = raw.data.streams.length
           ? raw.data.streams[0].channel
-          : null;
+          : null
 
         if (isStream) {
-          const {
-            name, logo, url, display_name, status, game, _id
-          } = isStream;
+          const { name, logo, url, display_name, status, game, _id } = isStream
           res.json({
             _id,
             stream: name,
@@ -136,25 +132,22 @@ app
             url,
             title: display_name,
             status,
-            game,
-          });
+            game
+          })
         }
       })
-      .catch(err => console.log('Err getting tourney data', err));
+      .catch(err => console.log('Err getting tourney data', err))
   })
 
   .get('/api/top-streams', async (req, res) => {
-    const topRaw = await fetch(
-      'https://api.twitch.tv/helix/streams?limit=12',
-      {
-        headers: {
-          'Client-ID': clientID
-        },
-      },
-    );
-    const top = await topRaw.json();
+    const topRaw = await fetch('https://api.twitch.tv/helix/streams?limit=12', {
+      headers: {
+        'Client-ID': clientID
+      }
+    })
+    const top = await topRaw.json()
 
-    res.json(top);
+    res.json(top)
   })
 
-  .listen(process.env.PORT || 4321, () => console.log('Server up!'));
+  .listen(process.env.PORT || 4321, () => console.log('Server up!'))
