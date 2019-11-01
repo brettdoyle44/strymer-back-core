4c5b7913908117be38f78cb17f3d7dc7d2d6cf4c
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-11-01 14:31:12 -0400

    Initial commit with updated Twitch API

---
+++ auth.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const request = require('request');
+const cors = require('cors');
+
+const { OAuth2Strategy } = require('passport-oauth');
+
+const TWITCH_CLIENT_ID = '1goezo7ijjizi65ftuafta782iuipv';
+const TWITCH_SECRET = 'nd94ig7ln2gslg090ks9k45eiustww';
+const SESSION_SECRET = 'justputitrightinthere';
+const CALLBACK_URL = 'https://strymer.stream/api/auth/twitch/callback';
+
+export const twitchAuth = express();
+twitchAuth.use(
+  session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }),
+);
+twitchAuth.use(express.static('public'));
+twitchAuth.use(passport.initialize());
+twitchAuth.use(passport.session());
+
+OAuth2Strategy.prototype.userProfile = (accessToken, done) => {
+  const options = {
+    url: 'https://api.twitch.tv/helix/users',
+    method: 'GET',
+    headers: {
+      'Client-ID': TWITCH_CLIENT_ID,
+      Accept: 'application/vnd.twitchtv.v5+json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  request(options, (error, response, body) => {
+    if (response && response.statusCode === 200) {
+      done(null, JSON.parse(body));
+    } else {
+      done(JSON.parse(body));
+    }
+  });
+};
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  'twitch',
+  new OAuth2Strategy(
+    {
+      authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+      tokenURL: 'https://id.twitch.tv/oauth2/token',
+      clientID: TWITCH_CLIENT_ID,
+      clientSecret: TWITCH_SECRET,
+      callbackURL: CALLBACK_URL,
+      state: true,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('profile', profile);
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+
+      done(null, profile);
+    },
+  ),
+);
+
+twitchAuth.get(
+  '/api/auth/twitch',
+  passport.authenticate('twitch', { scope: 'user_read' }),
+);
+
+twitchAuth.get(
+  '/api/auth/twitch/callback',
+  passport.authenticate('twitch', {
+    successRedirect: '/api/twitch/main',
+  }),
+);
+
+// needs to be strymer-test-new.herokuapp.com or production when deployed
+twitchAuth.get('/api/twitch/main', cors(), (req, res) => {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    res.redirect(
+      `https://strymer.gg/profile?${req.session.passport.user.data[0].id}--${
+        req.session.passport.user.accessToken
+      }`,
+    );
+  }
+});
---
+++ index.js
@@ -0,0 +1,160 @@
+import { } from 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { theIDs } from './ids.json';
+import { twitchAuth } from './auth';
+
+const fs = require('fs');
+// TWITCH APIS/IDS
+const api = require('twitch-api-v5');
+const axios = require('axios');
+const ffmpeg = require('fluent-ffmpeg');
+const fetch = require('node-fetch');
+
+const clientID = process.env.CLIENT_ID;
+const twitchStreams = require('twitch-get-stream')(clientID);
+
+const app = express();
+
+const corsOptions = [
+  'http://localhost:1234',
+  'https://strymer.gg',
+  'https://www.strymer.gg',
+];
+
+app
+  .use('*', cors(corsOptions))
+  .use(bodyParser.json())
+  .use(twitchAuth)
+  .get('/api/get-playlist', (req, res) => {
+    const { stream, quality } = req.query;
+
+    const getRawURL = streamName =>
+      twitchStreams
+        .rawParsed(streamName)
+        .then(back => back.filter(s => s.title.includes('audio_only'))[0].file)
+        .catch(err => {
+          console.log('ERROR GETTING TWITCH STREAM -- POSSIBLY PRIVATE?', err);
+        });
+
+    if (
+      fs.existsSync(
+        `/var/www/strymer.dev/html/${stream}-${quality || 'raw'}.m3u8`,
+      )
+    ) {
+      console.log('THIS PLAYLIST ALREADY EXISTS!');
+      res.json(`https://strymer.dev/${stream}-${quality || 'raw'}.m3u8`);
+    } else {
+      console.log('GETTING PLAYLIST FILE');
+
+      getRawURL(stream)
+        .then(async audioOnly => {
+          const audioToStream = !audioOnly
+            ? `/var/www/strymer.dev/html/${stream}.m3u8`
+            : audioOnly;
+
+          const bitRate = !quality ? 320 : quality;
+          ffmpeg(audioToStream, { timeout: 0 })
+            .audioBitrate(bitRate)
+            .noVideo()
+            .addOptions(['-hls_flags delete_segments'])
+            .on('error', err => console.log('ERRPR!@', err))
+            .save(`/var/www/strymer.dev/html/${stream}-${quality}.m3u8`);
+        })
+        .then(() => {
+          res.json(`https://strymer.dev/${stream}-${quality}.m3u8`);
+        })
+        .catch(err =>
+          console.log('There was an error sending the encoded file back', err),
+        );
+    }
+  })
+
+  .get('/api/meta', (req, res) => {
+    api.clientID = clientID;
+
+    const streamInfo = theIDs.map(id =>
+      axios
+        .get(`https://api.twitch.tv/helix/streams/${id}`, {
+          headers: {
+            'Client-ID': clientID
+          },
+        })
+        .then(v => v.data.stream),
+    );
+
+    Promise.all(streamInfo).then(streams =>
+      res.json(
+        streams
+          .filter(x => x)
+          .map(s => {
+            const {
+              name, logo, url, display_name, status, game
+            } = s.channel;
+
+            return {
+              stream: name,
+              logo,
+              game,
+              url,
+              title: display_name,
+              status,
+            };
+          }),
+      ),
+    );
+  })
+
+  .post('/api/tourney', (req, res) => {
+    const { search } = req.body;
+    console.log('SEA', search);
+
+    axios
+      .get(
+        `https://api.twitch.tv/helix/search/streams?query=${search.toLowerCase()}`,
+        {
+          headers: {
+            'Client-ID': clientID
+          },
+        },
+      )
+      .then(raw => {
+        console.log('RAW', raw);
+        const isStream = raw.data.streams.length
+          ? raw.data.streams[0].channel
+          : null;
+
+        if (isStream) {
+          const {
+            name, logo, url, display_name, status, game, _id
+          } = isStream;
+          res.json({
+            _id,
+            stream: name,
+            logo,
+            url,
+            title: display_name,
+            status,
+            game,
+          });
+        }
+      })
+      .catch(err => console.log('Err getting tourney data', err));
+  })
+
+  .get('/api/top-streams', async (req, res) => {
+    const topRaw = await fetch(
+      'https://api.twitch.tv/helix/streams?limit=12',
+      {
+        headers: {
+          'Client-ID': clientID
+        },
+      },
+    );
+    const top = await topRaw.json();
+
+    res.json(top);
+  })
+
+  .listen(process.env.PORT || 4321, () => console.log('Server up!'));
